ea59a58965b9f87d888582b722c02b7d5235b125
박정수 <jungsoo.park@devsisters.com>
2024-04-09 00:29:55 +0900

    강의 따라서 mongoDB 연결해보기

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
 const express = require('express')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://{0}:{1}@cluster0.i2ylb5e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
+String.prototype.format = function () {
+    var formatted = this;
+    for (var i = 0; i < arguments.length; i++) {
+        var regexp = new RegExp('\\{' + i + '\\}', 'gi');
+        formatted = formatted.replace(regexp, arguments[i]);
+    }
+    return formatted;
+};
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri.format(process.env.MONGODB_ID, process.env.MONGODB_PW), {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+let db
+client.connect().then((client) => {
+    console.log('DB연결성공')
+    db = client.db('forum')
+
+    app.listen(8080, () => {
+        console.log('http://localhost:8080 에서 서버 실행중')
+    })
+}).catch((err) => {
+    console.log(err)
 })
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (요청, 응답) => {
-    응답.send('반갑다')
-}) 
+    응답.sendFile(__dirname + '/index.html')
+})
+
+app.get('/list', async (요청, 응답) => {
+    let result = await db.collection('post').find().toArray()
+    console.log(result)
+})
